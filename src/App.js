ace980588f604acbeb779c889f4c0d71bf78d3e8
yhaappdev <121143197+yhaappdev@users.noreply.github.com>
2023-04-17 15:15:53 +0800

    updated

--- src/App.js
+++ src/App.js
@@ -1,119 +1,86 @@
-import './App.css';
-import QRCode from 'react-qr-code';
+import vCardsJS from "vcards-js";
+import { QRCodeSVG } from "qrcode.react";
 import { useState } from 'react';
-import vCardsJS from 'vcards-js';
+import './App.css';
+import { render } from "@testing-library/react";
+import React from 'react';
 
-function App() {
-  const [text, setText] = useState("");
+export default function App() {
+  var vCard = vCardsJS();
+  const [values, setValues] = useState({
+    firstName: '', lastName: '', organization: '', department: '', title: ''
+  });
 
-  function generateQR(e){
-    setText()
-  }
+  const set = firstName => {
+    return ({target: {value}}) => 
+      setValues(oldValues => ({...oldValues, [firstName]: value}));
+  };
 
-  function handleChange(e){
-    setText(e.target.value)
-  }
+  vCard.firstName = values.firstName;
+  vCard.lastName = values.lastName;
+  vCard.organization = values.organization + ';' + values.department;
+  vCard.workEmail = "";
 
-  function contactInfo(e){
-    var vCardJS = require('vcards-js');
- 
-    //create a new vCard
-    var vCard = vCardsJS();
+  vCard.cellPhone = "+";
+  vCard.workPhone = "+";
+  vCard.title = values.title;
+  vCard.role = values.title;
+  vCard.workUrl = "";
 
-    //set basic properties shown before
-    vCard.firstName = 'Eric';
-    vCard.middleName = 'J';
-    vCard.lastName = 'Nesser';
-    vCard.uid = '69531f4a-c34d-4a1e-8922-bd38a9476a53';
-    vCard.organization = 'ACME Corporation';
+  vCard.workAddress.label = "";
+  vCard.workAddress.street = "";
+  vCard.workAddress.city = "";
+  vCard.workAddress.postalCode = "";
+  vCard.workAddress.countryRegion = "";
 
-    //link to image
-    vCard.photo.attachFromUrl('https://avatars2.githubusercontent.com/u/5659221?v=3&s=460', 'JPEG');
+  const getNormalizeValue = (value) => {
+    let normalizedValue = value;
+    return normalizedValue;
+  };
 
-    //or embed image
-    vCard.photo.attachFromUrl('/path/to/file.jpeg');
+  const value = getNormalizeValue(vCard.getFormattedString());
 
-    vCard.workPhone = '312-555-1212';
-    vCard.birthday = new Date(1985, 0, 1);
-    vCard.title = 'Software Developer';
-    vCard.url = 'https://github.com/enesser';
-    vCard.workUrl = 'https://acme-corporation/enesser';
-    vCard.note = 'Notes on Eric';
+  console.log(vCard.getFormattedString());
+  console.log("value");
+  console.log(value);
 
-    //set other vitals
-    vCard.nickname = 'Scarface';
-    vCard.namePrefix = 'Mr.';
-    vCard.nameSuffix = 'JR';
-    vCard.gender = 'M';
-    vCard.anniversary = new Date(2004, 0, 1);
-    vCard.role = 'Software Development';
-
-    //set other phone numbers
-    vCard.homePhone = '312-555-1313';
-    vCard.cellPhone = '312-555-1414';
-    vCard.pagerPhone = '312-555-1515';
-
-    //set fax/facsimile numbers
-    vCard.homeFax = '312-555-1616';
-    vCard.workFax = '312-555-1717';
-
-    //set email addresses
-    vCard.email = 'e.nesser@emailhost.tld';
-    vCard.workEmail = 'e.nesser@acme-corporation.tld';
-
-    //set logo of organization or personal logo (also supports embedding, see above)
-    vCard.logo.attachFromUrl('https://avatars2.githubusercontent.com/u/5659221?v=3&s=460', 'JPEG');
-
-    //set URL where the vCard can be found
-    vCard.source = 'http://mywebpage/myvcard.vcf';
-
-    //set address information
-    vCard.homeAddress.label = 'Home Address';
-    vCard.homeAddress.street = '123 Main Street';
-    vCard.homeAddress.city = 'Chicago';
-    vCard.homeAddress.stateProvince = 'IL';
-    vCard.homeAddress.postalCode = '12345';
-    vCard.homeAddress.countryRegion = 'United States of America';
-
-    vCard.workAddress.label = 'Work Address';
-    vCard.workAddress.street = '123 Corporate Loop\nSuite 500';
-    vCard.workAddress.city = 'Los Angeles';
-    vCard.workAddress.stateProvince = 'CA';
-    vCard.workAddress.postalCode = '54321';
-    vCard.workAddress.countryRegion = 'United States of America';
-
-    //set social media URLs
-    vCard.socialUrls['facebook'] = 'https://...';
-    vCard.socialUrls['linkedIn'] = 'https://...';
-    vCard.socialUrls['twitter'] = 'https://...';
-    vCard.socialUrls['flickr'] = 'https://...';
-    vCard.socialUrls['custom'] = 'https://...';
-
-    //you can also embed photos from files instead of attaching via URL
-    vCard.photo.embedFromFile('photo.jpg');
-    vCard.logo.embedFromFile('logo.jpg');
-
-    vCard.version = '3.0'; //can also support 2.1 and 4.0, certain versions only support certain fields
-
-    //save to file
-    vCard.saveToFile('./eric-nesser.vcf');
-
-    //get as formatted string
-    console.log(vCard.getFormattedString());
-  }
-
-  return (
-    <div className="App">
-      <h1> QR Code Generator</h1>
-      <QRCode value={text}/>
-
-      <div className='input-here'>
-        <p>Enter text here</p>
-        <input type="text" value={text} onChange={(e) =>
-        {handleChange(e)}}/>
+    return (
+      <div className="App">
+        <form>
+          <div className = "vCardInfo">
+            <div>
+              <input type="text" id="firstName" value = {values.firstName} onChange={set('firstName')} />
+              <label htmlFor="firstName">First Name</label>
+            </div>
+            <div>
+              <input type="text" id="lastName" value = {values.lastName} onChange={set('lastName')} />
+              <label htmlFor="lastName">Last Name</label>
+            </div>
+            <div>
+              <input type="text" id="organization" value = {values.organization} onChange={set('organization')} />
+              <label htmlFor="organization">Organization</label>
+            </div>
+            <div>
+              <input type="text" id="department" value = {values.department} onChange={set('department')} />
+              <label htmlFor="department">Department</label>
+            </div>
+            <div>
+              <input type="text" id="title" value = {values.title} onChange={set('title')} />
+              <label htmlFor="title">Title</label>
+            </div>
+  
+          </div>
+        </form>
+        <QRCodeSVG
+          // TODO 
+          value={value}
+          size={200} // min 150
+          level="L"
+          // renderAs="canvas"
+          includeMargin={true}
+          bgColor="#fff"
+          fgColor="#000"
+        />
       </div>
-    </div>
-  );
+    );
 }
-
-export default App;
